830241b1bac1d9ac15d4b99d41f452b91a853258
Chirrise <2769692998@qq.com>
2024-12-03 16:12:52 +0800

    cesium相关代码 2024/12/03

---
+++ cesium/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <cesiumContainer/>
+</div>
+
+</template>
+
+<script>
+import cesiumContainer from "./components/cesiumContainer.vue"
+export default{
+  name:'App',
+  components:{
+    cesiumContainer
+  }
+}
+</script>
