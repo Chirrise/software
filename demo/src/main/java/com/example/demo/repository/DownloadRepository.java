881b0f2ac97720b7323e40c853b788353f523142
Chirrise <2769692998@qq.com>
2024-12-04 23:46:29 +0800

    Delete demo directory
    放错位置了

--- demo/src/main/java/com/example/demo/repository/DownloadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Download;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DownloadRepository extends JpaRepository<Download, Long> {
-}
